3878b7f3808d35cbff259a7246ffeedb876887db
Mark Schneider <schne112@purdue.edu>
2026-02-14 15:06:12 -0500

    remove unused imports

--- src/main/java/frc/robot/subsystems/Kicker.java
+++ src/main/java/frc/robot/subsystems/Kicker.java
@@ -2,9 +2,7 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
-import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.MotorAlignmentValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.wpilibj2.command.Command;
