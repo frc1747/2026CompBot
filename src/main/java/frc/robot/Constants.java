965ae816a2847e9c74e729aba752312f602ce889
Elliot Holmes <elliot.b.holmes@gmail.com>
2026-02-21 09:54:44 -0500

    adjusted constants indentation

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -20,46 +20,44 @@ import edu.wpi.first.math.numbers.N3;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+  public static final class Vision {
+    // Local hostnames of the unique Limelights on the system
+    // WARNING: IF YOU CHANGE OUT THE HARDWARE, ENSURE TO PROPERLY
+    // SET THE HOSTNAME ON THE LIMELIGHT TO COORESPOND WITH ITS 
+    // LOCATION ON THE BOT!!! 
+    public static final String LIMELIGHT_LEFT = "limelight-left";
+    public static final String LIMELIGHT_RIGHT = "limelight-right";
+    public static final String LIMELIGHT_TURRET = "limelight-turret";
 
-      public static final class Vision {
-        // Local hostnames of the unique Limelights on the system
-        // WARNING: IF YOU CHANGE OUT THE HARDWARE, ENSURE TO PROPERLY
-        // SET THE HOSTNAME ON THE LIMELIGHT TO COORESPOND WITH ITS 
-        // LOCATION ON THE BOT!!! 
-        public static final String LIMELIGHT_LEFT = "limelight-left";
-        public static final String LIMELIGHT_RIGHT = "limelight-right";
-        public static final String LIMELIGHT_TURRET = "limelight-turret";
+    // List of the active Limelights on the system to be used for Pose2D estimation
+    // Add any Limelights defined above to this list.
+    public static final List<String> ACTIVE_POSE_LIMELIGHTS = List.of(
+      LIMELIGHT_LEFT,
+      LIMELIGHT_RIGHT
+    );
 
-        // List of the active Limelights on the system to be used for Pose2D estimation
-        // Add any Limelights defined above to this list.
-        public static final List<String> ACTIVE_POSE_LIMELIGHTS = List.of(
-            LIMELIGHT_LEFT,
-            LIMELIGHT_RIGHT
-        );
+    // VISION_STDDEVS allows us to control how much we trust the values coming from the Limelight(s).
+    // The higher the value (distance standard deviations), the less we trust it.
+    // 
+    // n1: X Position Standard Deviations in meters
+    //     How wrong do we think vision could be about where we am on the field in X?
+    // n2: Y Position Standard Deviations in meters
+    //     How wrong do we think vision could be about where we are on the field in Y?
+    // n3: Rotation (theta) Standard Deviations in RADIANS
+    //     How wrong is vision about our heading?
+    //
+    // 0.7, 0.7, and 9999999 tells the code that we are somewhat trusting distant april tags
+    // and basically completely trusting the Pigeon for Yaw.
+    public static final Matrix<N3, N1> VISION_STDDEVS = VecBuilder.fill(0.7, 0.7, 9999999);
 
-        // VISION_STDDEVS allows us to control how much we trust the values coming from the Limelight(s).
-        // The higher the value (distance standard deviations), the less we trust it.
-        // 
-        // n1: X Position Standard Deviations in meters
-        //     How wrong do we think vision could be about where we am on the field in X?
-        // n2: Y Position Standard Deviations in meters
-        //     How wrong do we think vision could be about where we are on the field in Y?
-        // n3: Rotation (theta) Standard Deviations in RADIANS
-        //     How wrong is vision about our heading?
-        //
-        // 0.7, 0.7, and 9999999 tells the code that we are somewhat trusting distant april tags
-        // and basically completely trusting the Pigeon for Yaw.
-        public static final Matrix<N3, N1> VISION_STDDEVS = VecBuilder.fill(0.7, 0.7, 9999999);
+    public static final double FIELD_CENTER_X = 8.7741252;
+    public static final double FIELD_CENTER_Y = 4.0259508;
+  }
 
-        public static final double FIELD_CENTER_X = 8.7741252;
-        public static final double FIELD_CENTER_Y = 4.0259508;
-
-    }
-
-    public static final class Controller {
-        public static final int DRIVER_PORT = 0;
-        public static final int OPERATOR_PORT = 1;
-    }
+  public static final class Controller {
+    public static final int DRIVER_PORT = 0;
+    public static final int OPERATOR_PORT = 1;
+  }
 
   public static final class Drivetrain {
     public static final double MAX_SPEED = 4.1 * 0.5;  // Max speed in m/s  half-speed for now
