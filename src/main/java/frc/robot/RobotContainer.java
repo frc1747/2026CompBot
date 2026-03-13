983b030bb9d1b558fb4b615985f2fd5c007f1990
Mark Schneider <schne112@purdue.edu>
2026-03-13 17:18:39 -0400

    fix indentation

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -163,12 +163,16 @@ public class RobotContainer {
         operator.a().whileTrue(new AutoAim(shooter, hood, Constants.Shooter.RED_HUB_CENTER_POSE2D).andThen(kicker.run())).onFalse(kicker.stop());
 
         if (DriverStation.getAlliance().isPresent() && DriverStation.getAlliance().get() == Alliance.Red) {
-            operator.a().whileTrue(new AutoAim(shooter, hood, Constants.Shooter.RED_HUB_CENTER_POSE2D)).onFalse(shooter.stopCommand().alongWith(hood.stopCommand()));
+            operator.a()
+                .whileTrue(new AutoAim(shooter, hood, Constants.Shooter.RED_HUB_CENTER_POSE2D))
+                .onFalse(shooter.stopCommand().alongWith(hood.stopCommand()));
+        } else{
+            operator.a()
+                .whileTrue(new AutoAim(shooter, hood, Constants.Shooter.BLUE_HUB_CENTER_POSE2D))
+                .onFalse(shooter.stopCommand().alongWith(hood.stopCommand()));
+        }
     }
-        else{
-            operator.a().whileTrue(new AutoAim(shooter, hood, Constants.Shooter.BLUE_HUB_CENTER_POSE2D)).onFalse(shooter.stopCommand().alongWith(hood.stopCommand()));
-    }
-    }
+
     public Command getAutonomousCommand() {
         return autoChooser.getSelected();
     }
