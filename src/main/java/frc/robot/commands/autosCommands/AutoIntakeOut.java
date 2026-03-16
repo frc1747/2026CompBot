f8c2128b570ba55a4a0ad99b16c01c487ceb0ef1
rosemary <yeetmasterpobddb@gmail.com>
2026-03-16 18:19:18 -0400

    Finally Fixed Autos By adding (addRequirements) to the files

--- src/main/java/frc/robot/commands/autosCommands/AutoIntakeOut.java
+++ src/main/java/frc/robot/commands/autosCommands/AutoIntakeOut.java
@@ -1,20 +1,24 @@
 package frc.robot.commands.autosCommands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.IntakePivot;
 
 public class AutoIntakeOut extends Command {
     private IntakePivot intakePivot;
     private int tick;
+    private Timer timer = new Timer();
 
     public AutoIntakeOut(IntakePivot intakePivot, int tick) {
         this.intakePivot = intakePivot;
         this.tick = tick;
-
+        addRequirements(intakePivot);
     }
 
     @Override
     public void initialize() {
+        timer.reset();
+        timer.start();
         intakePivot.intakePivot(tick);
     }
 
@@ -27,7 +31,8 @@ public class AutoIntakeOut extends Command {
     public void end(boolean interrupted) {
         intakePivot.intakePivot(0);
     } 
+    @Override
     public boolean isFinished() {
-        return false;
+        return timer.get() > 1.0; // run for 1 second
     }
 }
