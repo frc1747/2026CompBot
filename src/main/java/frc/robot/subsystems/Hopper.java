3b71390acdc6a7c4ef3bf53eecc8f94cee945648
Cody Nissan <cnih150@gmail.com>
2026-03-17 18:43:42 -0400

    Puts if its reversed in elastic. Yeah. Yay!

--- src/main/java/frc/robot/subsystems/Hopper.java
+++ src/main/java/frc/robot/subsystems/Hopper.java
@@ -4,6 +4,7 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -66,7 +67,9 @@ public class Hopper extends SubsystemBase {
     }
 
     @Override
-    public void periodic() {}
+    public void periodic() {
+        SmartDashboard.setDefaultBoolean("hopper/is motor reversed", reversed());
+    }
 
 }
 
