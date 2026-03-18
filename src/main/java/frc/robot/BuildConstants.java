90aa1c83205ee9fa33d06bb4c52dd108be4c0574
rosemary <yeetmasterpobddb@gmail.com>
2026-03-18 19:39:28 -0400

    WIP: Shoot Auto and fix main

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,15 +5,15 @@ package frc.robot;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "2026CompBot";
+  public static final String MAVEN_NAME = "2026CompBot-3";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 194;
-  public static final String GIT_SHA = "placeholder";
-  public static final String GIT_DATE = "placeholder";
-  public static final String GIT_BRANCH = "placeholder";
-  public static final String BUILD_DATE = "placeholder";
-  public static final long BUILD_UNIX_TIME = 1773870371830L;
-  public static final int DIRTY = 0;
+  public static final int GIT_REVISION = 228;
+  public static final String GIT_SHA = "d792a878e1576f2651b9fd58f209f4c51f87d665";
+  public static final String GIT_DATE = "2026-03-18 19:11:21 EDT";
+  public static final String GIT_BRANCH = "newAutos";
+  public static final String BUILD_DATE = "2026-03-18 19:23:40 EDT";
+  public static final long BUILD_UNIX_TIME = 1773876220188L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
