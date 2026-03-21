e8a98fe7d4aa44e332b637ff8d14f58d1fb1b394
Braden Mosier <bmosi77@tscstudents.net>
2026-03-20 22:22:46 -0400

    some more changes

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -160,7 +160,7 @@ public final class Constants {
     public static final double SURFACE_E = -10.8847; // needs tuning
     public static final double SURFACE_F = 2.9962; // needs tuning
     public static final double MAX_AUTOSHOOT_POWER =4500;
-    public static final int AUTO_SHOOTER_MULT = 80; // this should be remove when we get better auto shoot values 
+    public static final int AUTO_SHOOTER_MULT = 112; // this should be remove when we get better auto shoot values 
     public static final double MAX_HOOD_ANGLE = 43;// degrees
     public static final double MIN_HOOD_ANGLE = 26;// degrees
     public static final double PID_P = .75;// they are half for the two motors 
