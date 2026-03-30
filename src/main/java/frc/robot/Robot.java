3409742c6892b828148efa7bcd6de3f1a33a9f9e
Cody <cnih150@gmail.com>
2026-03-30 19:57:46 -0400

    added monologue.updateAll() in Robot

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,6 +10,7 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import monologue.Monologue;
 
 public class Robot extends TimedRobot {
     private Command m_autonomousCommand;
@@ -44,6 +45,7 @@ public class Robot extends TimedRobot {
     @Override
     public void robotPeriodic() {
         CommandScheduler.getInstance().run();
+        Monologue.updateAll();
     }
 
     @Override
