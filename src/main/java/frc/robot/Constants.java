f32411ff6825289e4cbcc2ff78ca53cd86cdb06b
Isabelle Seliger <isabelle.seliger@gmail.com>
2026-04-07 20:01:28 -0400

    Changed april lock PIDs so that turret moves a little faster. The values are both 0.005 right now, but originally, they were both 0.003. There's a comment in the code stating that, in case it breaks. NEEDS TESTED ON BOT!!!

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -43,8 +43,8 @@ public final class Constants {
 
         // AprilLock2 rotation compensation pid values
         public static final double APRIL_LOCK_P = 1.0;
-        public static final double APRIL_LOCK_I = 0.003;
-        public static final double APRIL_LOCK_D = 0.003;
+        public static final double APRIL_LOCK_I = 0.005; // was 0.003
+        public static final double APRIL_LOCK_D = 0.005; // was 0.003
         // maximum magnitude of PID output
         public static final double APRIL_LOCK_PID_CLAMP = 0.5; // was .5
 
