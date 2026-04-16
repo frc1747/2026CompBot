f20d9c9f02d3b8d43966fcdd296fd6405791a689
rosemary <yeetmasterpobddb@gmail.com>
2026-04-15 20:53:40 -0400

    Mark told me to do this

--- src/main/java/frc/robot/commands/autosCommands/AutoAutoAim.java
+++ src/main/java/frc/robot/commands/autosCommands/AutoAutoAim.java
@@ -9,7 +9,6 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
 import frc.robot.RobotContainer;
-import frc.robot.TargetPoses;
 import frc.robot.subsystems.Hood;
 import frc.robot.subsystems.Shooter;
 
